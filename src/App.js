83f94837d7c06ae1a2fd612931b7c461a488b511
Ron Israeli <stamdvarim1000@gmail.com>
2021-01-14 13:47:14 +0200

    new

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import './App.css';
 import Form from './components/Form';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 
 const App = () => {
+
+  const useStylesFooter = makeStyles((theme) => ({
+    footer: {
+        backgroundColor:
+            theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[800],
+    },
+}));
+const classesFooter = useStylesFooter();
+
   return (
     <div className="all">
+    <div>
       <Form />
     </div>
+     <footer className={classesFooter.footer} style={{ textAlign: "center" }}>
+     <Container maxWidth="sm">
+         <Typography variant="body1" style={{ fontSize: "0.9rem" }}>ליצירת קשר יש לפנות למספר הפלאפון - 0522543225</Typography>
+     </Container>
+ </footer>
+ </div>
   );
 }
 
