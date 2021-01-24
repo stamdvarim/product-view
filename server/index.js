7ce6bd4fbb5612dc1467915be16e4258a1572d0a
Ron Israeli <stamdvarim1000@gmail.com>
2021-01-24 19:54:34 +0200

    nhjkhj

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const PORT = process.env.port || 8080;
+app.use(express.static(path.join(__dirname + '/../client', 'build')));
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname + '/../client', 'build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`server run on ${PORT}`);
+});
