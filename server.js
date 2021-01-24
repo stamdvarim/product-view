a4f144ecd1843003b9be7390badbcb8ccbca1a4d
RonIsraeli123 <63198823+RonIsraeli123@users.noreply.github.com>
2021-01-24 21:02:21 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,13 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-const PORT = process.env.PORT || 8080;
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`server run on ${PORT}`);
-});
+app.listen(process.env.PORT || 5000)
